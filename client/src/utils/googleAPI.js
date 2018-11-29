27ff04605a75ab97ed95701fbffa7eada48e27fd
Cmoney <cascherer42@gmail.com>
2018-11-29 11:10:57 -0600

    heroku test

--- client/src/utils/googleAPI.js
+++ client/src/utils/googleAPI.js
@@ -1,13 +1,13 @@
 import axios from "axios";
-import runtimeEnv from '@mars/heroku-js-runtime-env';
-const env = runtimeEnv();
-const api_key = env.REACT_APP_GOOGLE_BOOKS_API_KEY || process.env.REACT_APP_GOOGLE_BOOKS_API_KEY;
+// import runtimeEnv from '@mars/heroku-js-runtime-env';
+// const env = runtimeEnv();
+// const api_key = env.REACT_APP_GOOGLE_BOOKS_API_KEY || process.env.REACT_APP_GOOGLE_BOOKS_API_KEY;
 
 export default {
     // Search the book with the given id
     searchBook: (id) => {
 
-        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${id}&key=${api_key}`);
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${id}`);
     }
 
 };
